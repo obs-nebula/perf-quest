6fc02964207e6f5599c38fa653d4f1ffb6011466
Helio Frota <00hf11@gmail.com>
2023-01-23 14:52:25 -0300

    refactor: using typescript

---
+++ function/src/bench1.ts
@@ -0,0 +1,14 @@
+import { callDoWorkDisabled } from './disabled';
+import { callDoWorkEnabled } from './enabled';
+import bench from 'bench';
+
+exports.compare = {
+  'DISABLED': function () {
+    callDoWorkDisabled();
+  },
+  'ENABLED': function () {
+    callDoWorkEnabled();
+  }
+};
+
+bench.runMain();
--- function/src/disabled.ts
+++ function/src/disabled.ts
@@ -1,15 +1,11 @@
 function doWork() {
   for (let i = 0; i <= Math.floor(Math.random() * 40000000); i += 1) {
-
+    // 
   }
 }
 
-function callDoWork() {
+export function callDoWorkDisabled() {
   for (let i = 0; i < 10; i += 1) {
     doWork();
   }
-}
-
-module.exports = {
-  callDoWork
 }
